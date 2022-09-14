9c2c2dea0cbd67d51ae6cb526b2b5c1174a12533
Neelesh Ranjan <neeleshranjan95@gmail.com>
2022-09-14 17:22:49 +0530

    React Assignment 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import ClickCounter from './Components/ClickCounter';
-import HoverCounter from './Components/HoverCounter';
 
 function App() {
   return (
     <div>
       <ClickCounter />
-     
       
     </div>
   );
